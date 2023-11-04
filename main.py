a9b524cd3af7c8ade4401c4ad03242c43792a60c
BaAniq <basia.anisimowicz@gmail.com>
2023-11-04 11:29:30 +0100

    Adding a window asking for a state's name

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import turtle
-
+image = "blank_states_img.gif"
 screen = turtle.Screen()
 screen.title('U.S States Game')
 
-screen.addshape("blank_states_img.gif")
+screen.addshape(image)
 
-turtle.shape('blank_states_img.gif')
+turtle.shape(image)
+screen.textinput(title='Guess the State', prompt="What's another state's name")
 
 
 screen.exitonclick()
