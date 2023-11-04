32a0536b6499a13c3afb61e715179998cc83d5de
BaAniq <basia.anisimowicz@gmail.com>
2023-11-04 13:00:22 +0100

    Print the guessed state's name on screen

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import turtle
+from turtle import Turtle, Screen
+import pandas
 image = "blank_states_img.gif"
-screen = turtle.Screen()
+screen = Screen()
 screen.title('U.S States Game')
-
 screen.addshape(image)
+screen_t = Turtle()
+screen_t.shape(image)
 
-turtle.shape(image)
-screen.textinput(title='Guess the State', prompt="What's another state's name")
+states_and_data = pandas.read_csv('50_states.csv')
+users_state = (screen.textinput(title='Guess the State', prompt="What's another state's name")).title()
+
+data_for_users_state = states_and_data[states_and_data.state == users_state]
+x_user_state = int(data_for_users_state.x)
+y_user_state = int(data_for_users_state.y)
+
+state = Turtle()
+state.pu()
+state.shape('circle')
+state.shapesize(0.1, 0.1)
+state.goto(x_user_state, y_user_state)
+state.write(users_state)
 
 
 screen.exitonclick()
