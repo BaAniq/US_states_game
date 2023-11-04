56891b486cb98a80b43fdd34f0c9d2b1201e2018
BaAniq <basia.anisimowicz@gmail.com>
2023-11-04 13:47:39 +0100

    Keep the window open and count right guesses

--- main.py
+++ main.py
@@ -9,19 +9,22 @@ screen_t.shape(image)
 
 states_and_data = pandas.read_csv('50_states.csv')
 states_name_list = states_and_data.state.to_list()
-users_state = (screen.textinput(title='Guess the State', prompt="What's another state's name")).title()
+guessed_states = []
+while len(guessed_states) < 50:
+    users_state = (screen.textinput(title=f'{len(guessed_states)}/50 Guess the State', prompt="What's another state's name")).title()
 
-if users_state in states_name_list:
-    data_for_users_state = states_and_data[states_and_data.state == users_state]
-    x_user_state = int(data_for_users_state.x)
-    y_user_state = int(data_for_users_state.y)
+    if users_state in states_name_list:
+        guessed_states.append(users_state)
+        data_for_users_state = states_and_data[states_and_data.state == users_state]
+        x_user_state = int(data_for_users_state.x)
+        y_user_state = int(data_for_users_state.y)
 
-    state = Turtle()
-    state.pu()
-    state.shape('circle')
-    state.shapesize(0.1, 0.1)
-    state.goto(x_user_state, y_user_state)
-    state.write(users_state)
+        state = Turtle()
+        state.pu()
+        state.shape('circle')
+        state.shapesize(0.1, 0.1)
+        state.goto(x_user_state, y_user_state)
+        state.write(users_state)
 
 
 screen.exitonclick()
